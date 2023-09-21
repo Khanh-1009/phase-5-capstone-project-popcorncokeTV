cf59ca6c0e952fafcba1735e01baff49b6aca9c6
Khanh Tran <katetrannbkhanh@gmail.com>
2023-09-21 14:25:17 -0700

    Adding controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  before_action :authorize
+
+  def authorize 
+    render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id        
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
